91ca4461e4db2a5ec33a48dbefa0b850e4ff3d3a
Aarav <127654235+CodesArv@users.noreply.github.com>
2024-06-21 16:02:50 +0530

    Update CatgoreyRoute.js
    changes done

--- routes/CatgoreyRoute.js
+++ routes/CatgoreyRoute.js
@@ -5,7 +5,6 @@ const {
   createCategory,
   updateCategory,
   deleteCategory,
-  getCategoryDetails,
 } = require("../controllers/categoryControllers");
 const singleUpload = require("../middleware/multer");
 
@@ -16,7 +15,7 @@ router.route("/getCategory").get(getAllCategory);
 
 //creat new category
 router
-  .route("/admin/category/new").post(isAuthanticatedUser, authorizedRole("admin"),singleUpload,createCategory);
+  .route("/admin/category/new").post(isAuthanticatedUser, authorizedRole("admin"), singleUpload, createCategory);
 
 router
   .route("/Category/:id")
